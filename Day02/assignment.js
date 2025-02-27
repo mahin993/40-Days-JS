930f98205a49aab5882ff9e5708976b0de36db29
mahin993 <mahincou93@gmail.com>
2025-02-28 03:15:58 +0900

    Day2 Practice and Complete assignment 2 Tasks

---
+++ Day02/assignment.js
@@ -0,0 +1,15 @@
+/*
+  👩‍💻 🧑‍💻 Assignment Tasks
+  ✅ Task 1: Declare variables for a person’s name, age, isStudent status, and favorite programming language.
+  ✅ Task 2: Print the values to the console.
+  ✅ Task 3: Try reassigning values to let and const variables and observe errors.
+  ✅ Task 4: Create an object and an array, assign them to new variables, modify, and observe changes.
+*/
+
+//Task 1:
+let name = "Mahin";
+let age = 32;
+let isStudent = false;
+let favoriteProgrammingLanguage = "JavaScript";
+
+console.log(name, age, isStudent, favoriteProgrammingLanguage);
--- Day02/index.js
+++ Day02/index.js
@@ -1,4 +1,43 @@
-console.log("Hello JavaScript")
+console.log("Day 02");
 
-document.getElementById("someId").innerText = "I am some div";
-document.write("Hello")
+// Variables: Variables are used to store data in JavaScript.
+
+// syntax of variable:
+// let/const storage_name = value
+
+
+// - 'var': Function-scoped, can be redeclared (not recommended)
+// - 'let': Blocked-scoped, can be reassigned
+// - 'const': Blocked-scoped, **cannot** be reassigned
+
+const address = "Bangladesh"
+console.log(address)
+
+
+console.log(address)
+
+console.log();
+
+
+/*
+- **Primitive Data Types: **
+- `String` - Text values (`"Hello"`)
+- `Number` - Numeric values (`25`, `3.14`)
+- `Boolean` - True/False (`true`, `false`)
+- `Undefined` - A variable declared but not assigned (`let x`)
+- `Null` - Represent "nothing" (`let y = null;`)
+- `BigInt` - Large identifiers (`BigInt (12345678901234567890)`)
+- `Symbol` - Unique identifiers (`Symbol("id")`)
+
+- **Non-Primitive (Reference) Data Types: **
+- `Object` - Collection of key-value pairs
+- `Array` - Ordered list of values
+- `Function` - Code that can be executed
+*/
+
+let student = {
+  name: "Alice",
+  age: 22,
+  isEnrolled: true
+};
+console.log(student.name);
