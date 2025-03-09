7fd1ff1d634135da63f8db9a1b9301dfe4b00345
ayesha akhy <akhyemc@gmail.com>
2025-03-09 11:21:49 +0900

    Module 6' Function Practice

---
+++ Day06/index.js
@@ -0,0 +1,15 @@
+// Definition or Declaration of a Function
+function printMe(){
+    console.log("printing...")
+}
+
+// call or Invoke a Function
+printMe()
+
+
+// Expression of a Function
+let printMe2 = function(){
+    console.log("Printing 2 ...");
+}
+
+printMe2()
