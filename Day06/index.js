d96e728fc8062f3f0fe3e19dc1004be7d2a0addb
mahin993 <mahincou93@gmail.com>
2025-03-11 05:24:03 +0900

    Learn Parameters, Arguments, Return Statement, Default Parameter, Rest Parameter

--- Day06/index.js
+++ Day06/index.js
@@ -1,6 +1,6 @@
 // Definition or Declaration of a Function
 function printMe(){
-    console.log("printing...")
+    // console.log("printing...")
 }
 
 // call or Invoke a Function
@@ -9,7 +9,49 @@ printMe()
 
 // Expression of a Function
 let printMe2 = function(){
-    console.log("Printing 2 ...");
+    // console.log("Printing 2 ...");
 }
 
 printMe2()
+
+/*
+ Parameters & Arguments
+
+Parameter: The input that you pass to a function is called Parameter.
+The input that we pass to a function while declaring or defining function is call Parameters.
+
+Arguments: When we invoke or call this particular function and that time passing the actual value of Parameters (a, b) is call Arguments.
+
+*/
+function sum(a, b){
+    const result = a + b;
+    // console.log(result);
+    return result;
+}
+
+let result = sum(5, 6);
+// console.log(result);
+
+function double(x){
+    return x * 2;
+}
+
+// console.log(double(result));
+
+
+// Default Parameter: 
+// Default Parameter means a Parameter, which is having a default value.
+function calc(a, b=0){ // b=0 is a default parameter
+    return 2 * (a + b);
+}
+
+let result2 = calc(2);
+// console.log(result2);
+
+
+// Rest Parameter
+function calculateThis(x, y, ...rest){
+    console.log(x, y, rest);
+}
+
+calculateThis(1, 2, 3, 4, 5, 6, 7, 8, 9);
