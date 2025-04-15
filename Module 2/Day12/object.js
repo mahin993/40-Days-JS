68d191f3dabe212bd6345531e77acbdd446cdd3e
mahin993 <mahincou93@gmail.com>
2025-04-15 22:56:08 +0900

    Learn Objects Basic (Object,  Property-value pair, constructor function, factory function, Object Shorthand, “in” operator, for…in loop, Object Reference, )

---
+++ Module 2/Day12/object.js
@@ -0,0 +1,159 @@
+// Object
+
+let user = {
+  name: "Mahin",
+  age: 30,
+  "is admin": true
+};
+
+// console.log(user.name)
+// console.log(user.age);
+
+// add new property in particular object
+user.isSeniorCitizen = false;
+user["is_married"] = false;
+
+// console.log(user)
+// console.log(user.isSeniorCitizen)
+
+// console.log(user["is admin"])
+// console.log(user);
+
+// Modify the current property value
+user.age = 25
+// console.log(user.age); // 25
+
+// Delete the property
+// delete user.age;
+// delete user["is admin"]
+// console.log(user);
+
+
+// access property value dynamically or by using variable
+const somekey = "age";
+// console.log(user[somekey]); // age = 25
+
+
+
+// access property name dynamically or by using variable
+// let car = prompt("Which is your fav car ?")
+
+let favCars = {
+  // [car]: 5,
+}
+
+// console.log(favCars);
+
+
+//  Constructor Function 
+// Note that, Constructor Function name will be start with a capital letter.
+function Car(name, model){
+  this.name = name;
+  this.model = model;
+}
+
+const bmwCar = new Car("BMW", "X1");
+// const audiCar = new Car("AUDI", "A8");
+// console.log(bmwCar); // Car { name: 'BMW', model: 'X1' }
+// console.log(audiCar); // Car { name: 'AUDI', model: 'A8' }
+// Constructor Function helps out to create different instances from blueprint that you use to create different instances
+
+
+// console.log(bmwCar instanceof Car);
+
+
+
+// Create Object using "Object" keyword
+const person = Object();
+person.name = "Alpha";
+person.age = 76;
+person["is_married"] = true;
+
+// console.log(person);
+// console.log(person);
+
+// Factory Function:
+function createUser(name, age){
+  return {
+    // Object Shorthand: Object shorthand is a short way to write objects in JavaScript when the property name and variable name are the same.
+    name, // name: name
+    age, // age: age
+    greet(){
+      // console.log(this.name);
+      
+    }
+  }
+}
+
+const user1 = createUser("Mahin", 32)
+// console.log(user1);
+user1.greet()
+
+const user2 = createUser("Tuhin", 25)
+// console.log(user2);
+
+
+// Nested Object
+let profile = {
+  name: "Mahin",
+  company: "Meitec",
+  message: function(){
+    console.log(`${this.name} works at ${this.company}`);
+  },
+  address: {
+    city: "Chandpur",
+    thana: "Kachua",
+    post_code: 3630,
+    state: "Chittagong",
+    country: "Bangladesh",
+    greeting: function(){
+      console.log(`Welcome to ${this.country}`);
+    },
+  },
+  salary: 20000,
+}
+
+// console.log(profile.name);
+// console.log(profile.company);
+
+// profile.message();
+
+// console.log(profile.address.city);
+// console.log(profile.address.greeting());
+// profile.address.greeting()
+
+console.log(profile.salary);
+if(!profile.salary){
+  // console.log("Salary Doesn't exist");
+}
+
+// "in" operator to check the property is exist in object or not
+// console.log("salary" in profile) // false
+
+
+
+//for……in loop
+
+for(key in profile){
+  // console.log(key);
+  // console.log(profile[key]);
+}
+
+// console.log(Object.keys(profile));
+// console.log(Object.values(profile));
+
+
+// Object Reference
+// const obj1 = {name: "mango"}; // XA01
+// const obj2 = {name: "mango"}; // VB02
+
+// console.log(obj1 === obj2); // false
+// console.log(obj1 == obj2); // false
+
+
+let obj1 = {name: "mango"}; // XA01
+const obj2 = {name: "mango"}; // VB02
+
+obj1 = obj2
+// console.log(obj1 === obj2); // true
+// console.log(obj1 == obj2); // true
