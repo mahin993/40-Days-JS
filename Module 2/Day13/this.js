d94dc4f5e778b90d485a80a867dffb125feb9505
mahin993 <mahincou93@gmail.com>
2025-04-23 03:29:19 +0900

    learnt Implicit Binding of "this" keyword

--- Module 2/Day13/this.js
+++ Module 2/Day13/this.js
@@ -10,6 +10,9 @@
 
 // Inside of an Object - Implicit Binding
 // When invoke a method on an object the context of "this" or the value of "this" is bound to the object on which we have invoked the method
+
+
+// "use strict";
 const person = {
   id: 12345,
   firstName: "Alex",
@@ -41,10 +44,10 @@ const jerry = {
 
 function greetMe(obj){
   obj.logMessage = function(){
-    console.log(`${this.name} is ${this.age} years old!`);
+    // console.log(`${this.name} is ${this.age} years old!`);
   }
 
-  console.log(obj);
+  // console.log(obj);
 }
 
 greetMe(tom)
@@ -52,3 +55,56 @@ tom.logMessage()
 
 greetMe(jerry)
 jerry.logMessage()
+
+
+
+//  Inside Function
+function sayName(){
+  console.log("this inside a function", this); // window object
+}
+
+// sayName()
+
+function sayName() {
+  console.log(this); // undefined
+}
+// sayName();
+
+
+// "this" inside nested functions
+
+function outer(a){
+  // console.log("this inside an outer function", this); // window obj
+
+  return function inner(b){
+    // console.log("this inside an inner function", this); // window obj
+  }
+}
+
+const outerResult = outer(); // Call outer()
+// console.log(outerResult()); // Call inner()
+
+
+
+// "this" inside the arrow function
+// 
+const getFood = () => this;
+// console.log("this inside the arrow function defined in global scope", getFood());
+
+
+
+const food = {
+  name: "mango",
+  color: "yellow",
+
+  // getDesc: function() {
+  //   return `${this.name} is ${this.color}`
+  // }
+  getDesc: function(){
+    return () => `${this.name} is ${this.color}`
+  }
+} 
+
+const descFunc = food.getDesc()
+console.log(descFunc());
+
