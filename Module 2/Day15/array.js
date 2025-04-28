19aab01be387ed212eca3396fddaf8a7c9d93ce4
mahin993 <mahincou93@gmail.com>
2025-04-29 04:00:05 +0900

    Array of JS

---
+++ Module 2/Day15/array.js
@@ -0,0 +1,9 @@
+/**
+ * Array:
+ * Index: The position of an element in the array is known as its index.
+ * Index starts with 0
+ * Index end with (length - 1)
+ */
+
+const two = new Array(4, 2)
+console.log(two);
