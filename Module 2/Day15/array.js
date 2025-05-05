82c7ee25ffd7cb0011c2c5a4100365e459766a6a
mahin993 <mahincou93@gmail.com>
2025-05-05 16:28:45 +0900

    Completed Array Methods and Tasks

--- Module 2/Day15/array.js
+++ Module 2/Day15/array.js
@@ -46,3 +46,103 @@ const array = [11, 21, 73]
 array.length = 7 // set array length
 array.length = 0 // set array length as empty array
 console.log(array);
+
+
+// Array Method
+
+// concat method
+const array1 = [1,2,3]
+const array2 = [4,5,6]
+
+const merged = array1.concat(array2)
+console.log(merged);
+
+
+// Join Method: The join method joins all the elements of an array by a separator like (comma, space, colon). Without any separator, as default join will be separate comma (,) comma.
+const alphabet = ["a", "b", "c", "d", "e"]
+const joined = alphabet.join(); // separate by (,) comma
+const joined2 = alphabet.join(" <=> "); // separate by (<=>)
+console.log(joined); // a,b,c,d,e
+
+console.log([].join()); // return "" (empty string)
+
+
+// fill() Method
+const colors = ["red", "blue", "green", "white", "black"]
+colors.fill("pink", 1, 3)
+console.log(colors); // ['red', 'pink', 'pink', 'white', 'black']
+
+
+// Sort Method: Sorts elements as strings by default, and the default sorting order is ascending
+// To sort numbers correctly, use a compare function or comparator function.
+const numbers = [4, 2, 10, 1];
+numbers.sort();
+console.log(numbers); // [1, 10, 2, 4] (not correct for numbers)
+
+numbers.sort(function(a, b){
+  return a === b ? 0 : a > b ? 1 : -1; // ascending order
+  return a === b ? 0 : a > b ? -1 : 1; // descending order
+});
+console.log(numbers); // [1, 2, 4, 10]
+
+
+const array3 = [1, 2, 3, 4, 5, 6, 7];
+array3.copyWithin(0, 4);
+console.log(array3); // [5, 6, 7, 4, 5, 6, 7]
+
+
+const items = [
+  { type: "fruit", name: "apple" },
+  { type: "veg", name: "carrot" },
+  { type: "fruit", name: "banana" }
+];
+
+const grouped = Object.groupBy(items, ({name}) => name);
+console.log(grouped);
+
+
+const inventory = [
+  { name: "asparagus", type: "vegetables", quantity: 5 },
+  { name: "bananas", type: "fruit", quantity: 0 },
+  { name: "goat", type: "meat", quantity: 23 },
+  { name: "cherries", type: "fruit", quantity: 5 },
+  { name: "fish", type: "meat", quantity: 22 },
+];
+
+
+const groupedByType = Object.groupBy(inventory, ({type}) => type)
+console.log(groupedByType);
+
+const groupedByQuantityMoreThan20 = Object.groupBy(inventory, ({quantity}) => {
+  return quantity >= 20 ? "Quantity More Than 20" : "Quantity Less Than 20";
+})
+
+
+
+// toReversed() Method
+{
+  const items = [1, 2, 3, 4, 5]
+  const reversed = items.toReversed()
+  console.log(reversed);
+  console.log(items);
+}
+
+// toSpliced() method
+const months = ["January", "March", "April", "May"]
+const months2 = months.toSpliced(1, 0, "February")
+console.log(months2); // // ["January", "February", "March", "April", "May"]
+console.log(months); // ["January", "March", "April", "May"]
+
+
+// with() Method
+// syntax: array.with(index, value) 
+// If index is negative value like (-1, -2 etc), then counting from the end
+{
+  const numbers = [1, 2, 3, 4, 5, 6]
+  const newArray = numbers.with(3, 7)
+  console.log(numbers);
+  console.log(newArray);
+
+  const anotherArray = numbers.with(-2, 8)
+  console.log(anotherArray); // // [1, 2, 3, 7, 8, 6] Counting from the last
+}
--- Module 2/Day15/tasks/task.js
+++ Module 2/Day15/tasks/task.js
@@ -57,3 +57,37 @@ for(let i=0; i<=numbers.length; i++){
   }
 }
 console.log(numbers);
+
+
+// T-014: What happens when you concatenate two empty arrays?
+{
+  const arr1 = []
+  const arr2 = []
+
+  const result = arr1.concat(arr2)
+  console.log(result); // []
+}
+
+// T-015: How can you check if a value is partially matching with any of the elements of an Array??
+const fruits = ["apple", "banana", "pineapple", "grape"];
+const isPartialMatch = fruits.some(fruit => fruit.includes("app"))
+console.log(isPartialMatch); // true
+
+
+// T-017: Create an Array of alphanumeric strings. Sort the elements in both ascending and descending orders. You must be doing this in an immutable way such that the source array never gets modified.
+{
+  const fruits = ["banana", "apple", "pineapple", "grape"];
+  const sorted = fruits.toSorted((a, b) => {
+    return a === b ? 0 : a > b ? 1 : -1; // Ascending
+    return a === b ? 0 : a > b ? -1 : 1; // Descending
+  })
+  console.log(sorted); //  ['pineapple', 'grape', 'banana', 'apple']
+  console.log(fruits); // ['banana', 'apple', 'pineapple', 'grape']
+}
+
+// T-019: Give a practical usages of the .fill() method
+{
+  const nums = [1, 2, 3, 4]
+const filled = nums.fill("one", 2, 3)
+console.log(filled);
+}
