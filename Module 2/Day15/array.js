490c90e84b5316a936295a7a50613f9e21a1a115
mahin993 <mahincou93@gmail.com>
2025-05-06 15:44:08 +0900

    Array Static Method (Array Like, Array.from(), Array.fromAsync(), Array.of() )

--- Module 2/Day15/array.js
+++ Module 2/Day15/array.js
@@ -146,3 +146,65 @@ console.log(months); // ["January", "March", "April", "May"]
   const anotherArray = numbers.with(-2, 8)
   console.log(anotherArray); // // [1, 2, 3, 7, 8, 6] Counting from the last
 }
+
+
+
+
+// Array Like
+const arr_like = {0: "I", 1: "am", 2: "array-like", length: 3};
+console.log(arr_like)
+console.log(arr_like[2]); // array-like
+console.log(arr_like.length); // 3
+
+console.log(`Is arr_like is an array ? ${Array.isArray(arr_like)}`); // false
+console.log(`Is arr_like is an object? ${arr_like instanceof Object}`); // true
+
+
+// How to Convert to Real Array:
+
+// Method 1:
+function checkArgs(){
+  console.log("Array Like Args: ", arguments);
+  const argArr = [...arguments]
+  console.log("Converted Array Args: ", argArr);
+  argArr.forEach((element) =>{
+    console.log(element);
+  })
+}
+
+checkArgs(1, 2, 3, 4, 5)
+
+
+// Method 2:
+const liList = document.getElementsByTagName('li');
+console.log(liList);
+const collectionArray = Array.from(liList);
+console.log("Converted Collection Array: ", collectionArray);
+collectionArray.forEach((element)=>{
+  console.log(element);
+})
+
+
+// Array.fromAsync()
+const collectionPromise = Array.fromAsync(document.getElementsByTagName('li'))
+console.log("Converted Array: ", collectionPromise);
+// collectionPromise.then((value) => console.log(value));
+
+
+const ret = Array.fromAsync({
+  0: Promise.resolve('TapaScript'),
+  1: Promise.resolve('Google'),
+  2: Promise.resolve('Apple'),
+  length: 3
+}).then((value)=>console.log(value))
+
+console.log(ret);
+
+
+
+// Array.of(): Array.of() is a static method that creates a new Array instance from a variable number of arguments, regardless of number or type of the arguments.
+const a = new Array(2, 3, 4); // [2, 3, 4]
+const b = [5, 6, 7]; // [5, 6, 7]
+
+const c = Array.of(2, true, 'test', {"name" : "Alex"}, [1,2,3]);
+console.log(c);
