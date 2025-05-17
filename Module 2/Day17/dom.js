b56b0e4b59c02d67e8e03c7e2c7b76f40e721ce3
mahin993 <mahincou93@gmail.com>
2025-05-18 01:19:03 +0900

    Basic Concepts of JS DOM (DOM, DOM Types)

---
+++ Module 2/Day17/dom.js
@@ -0,0 +1,22 @@
+/**
+ - What is DOM ?
+ - Understanding DOM Types    
+ - Accessing DOM
+
+
+  - Understanding DOM Types 
+ There are 6 types of DOM:
+        1. Document: Represents the entire page and it is the root node of the DOM tree.
+        console.log(document);
+
+        2. Node: Node is a generic term of any or elements or items of DOM tree. (Element Node, Text Node, Attribute Node)
+
+        3. Element: A specific type of node that represents HTML tags/elements
+        4. NodeList: An array of Nodes. It is an order list.
+
+        5. Attribute: Represents the attribute of a node.
+            <img src="/" alt="some image" />
+
+        6. NameNodeMap: A collection of Attribute.
+
+ */
