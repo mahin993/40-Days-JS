6e65dc84042db551c93341b69f48def4ac70b92c
mahin993 <mahincou93@gmail.com>
2025-05-18 22:00:45 +0900

    Rename Folder

---
+++ Module 1/Day07/secret-number/secret-number.js
@@ -0,0 +1,15 @@
+/*
+Let us play a number guessing game.
+You will ask user for a number between 1 to 10. Once user will enter a number, you will tell user if the entered number is lower of higher with this information, user will change the number and finally guess the right number. You also need to show the number of attempts made by users to rich to this right guess.
+*/
+
+/*
+- min number
+- max number
+- secret number is a random number between the min and max
+- get the prompt from the user as a guess
+- create a variable to store the number of attempts
+- write the logic to check if the guess and the secret number are same. If not continue a loop to ask again.
+- Inside loop check if the guess is less than the secret or greater than, accordingly tell if the guess it low or high.
+- Give the option to play again
+*/
