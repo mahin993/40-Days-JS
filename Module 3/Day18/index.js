a3f425a2c171af86c8859fdb03fb6bda75d54710
mahin993 <mahincou93@gmail.com>
2025-05-28 11:49:37 +0900

    DOM Manipulation (Modifying Content, romoving/replacing Elements)

--- Module 3/Day18/index.js
+++ Module 3/Day18/index.js
@@ -15,7 +15,7 @@
 {
 const pElem = document.createElement("p");
 pElem.innerText = "This is the text added dynamically"
-document.body.appendChild(pElem);
+// document.body.appendChild(pElem);
 console.log(pElem);
 }
 
@@ -24,7 +24,37 @@ const span = document.createElement("span");
 span.innerText = "This is span tag added dynamically";
 
 const targetElem = document.getElementById("title");
-document.body.insertBefore(span, targetElem)
+// document.body.insertBefore(span, targetElem)
 // document.body.insertBefore(span, targetElem.nextElementSibling);
 console.log(targetElem.nextElementSibling);
 
+
+{
+  // Modifying Content
+  const pElem = document.querySelector("p");
+  pElem.innerHTML = "<u>Hello How</u> are you doing?";
+
+  const divElem = document.querySelector("div");
+  console.log("Inner Text", divElem.innerText);
+  console.log("Text Content", divElem.textContent);
+  
+}
+
+
+{
+  // Removing/Replacing Elements
+  const list = document.getElementById("myList");
+  // console.log(list.children);
+  const itemRemove = list.children[0];
+  list.removeChild(itemRemove);
+
+  const pElem = document.querySelector("p");
+  list.replaceChildren(pElem)
+
+
+  const remove = document.getElementById('removeMe').remove();
+
+  
+  
+
+}
